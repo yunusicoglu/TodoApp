8122a8b7ce8e57a6daf97f00c3486bc273c2c107
yunusicoglu <yunusicoglu@gmail.com>
2024-03-31 13:16:26 +0300

    some css bugs edited

--- src/containers/Todo/TodoList.jsx
+++ src/containers/Todo/TodoList.jsx
@@ -28,30 +28,28 @@ const TodoList = ({newTodo}) => {
   
   return (
     <>
-      <Grid className='todo_list_section_main_grid'>
-        <Box className='todo_list_section_main_box' >
-          <Box sx={{ height:"50px", mb:"10px", pt:"15px", borderTopRightRadius:"25px", borderTopLeftRadius:"25px", display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
-            <Typography sx={{fontSize:"25px", fontWeight:600, ml:"20px"}} >Yapılacaklar Listesi</Typography>
-            <Divider sx={{bgcolor:"#ffdede",borderBottomWidth: 2}}/>
-          </Box>
-          <Box sx={{overflowY:"auto", width:"100%", height:"505px"}}>
-            {todoList&&todoList.map((todo) => (
-              <Box key={todo.id} sx={{ width:"100%", height:"40px", pt:"10px", pb:"0px", display:"flex", flexDirection:"column", justifyContent:"space-between",
-                ":hover":{bgcolor:"rgb(255, 255, 255, 0.3)"}}}>
-                <Box sx={{width:"100%" ,display:'flex',}}>
-                  <Typography sx={{ml:"20px",fontSize:"20px", width:"95%"}}>
-                    {todo.name}
-                  </Typography>
-                  <IconButton sx={{mt:"-6px", mr:"12px"}}>
-                    <ClearIcon/>
-                  </IconButton>
-                </Box>
-                <Divider sx={{mx:"15px"}}/>
-              </Box>
-            ))}
-          </Box>
+      <Box className='todo_list_section_main_box' >
+        <Box sx={{ height:"50px", mb:"10px", pt:"15px", borderTopRightRadius:"25px", borderTopLeftRadius:"25px", display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
+          <Typography sx={{fontSize:"25px", fontWeight:600, ml:"20px"}} >Yapılacaklar Listesi</Typography>
+          <Divider sx={{bgcolor:"#ffdede",borderBottomWidth: 2}}/>
         </Box>
-      </Grid>
+        <Box sx={{overflowY:"auto", width:"100%", height:"505px"}}>
+          {todoList&&todoList.map((todo) => (
+            <Box key={todo.id} sx={{ width:"100%", height:"40px", pt:"10px", pb:"0px", display:"flex", flexDirection:"column", justifyContent:"space-between",
+              ":hover":{bgcolor:"rgb(255, 255, 255, 0.3)"}}}>
+              <Box sx={{width:"100%" ,display:'flex',}}>
+                <Typography sx={{ml:"20px",fontSize:"20px", width:"95%"}}>
+                  {todo.name}
+                </Typography>
+                <IconButton sx={{mt:"-6px", mr:"12px"}}>
+                  <ClearIcon/>
+                </IconButton>
+              </Box>
+              <Divider sx={{mx:"15px"}}/>
+            </Box>
+          ))}
+        </Box>
+      </Box>
     </>
   )
 }
